38694f384f95d976cb0eccc1a19ac583457c7e97
ghostik57 <ghostik577@gmail.com>
2024-10-27 14:56:07 +0300

    Лабораторная работа 2

---
+++ Лабораторная работа 2/Task1.py
@@ -0,0 +1,17 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+months = 0
+
+while money_capital >= 0:
+    deficit = spend - salary
+    if deficit > 0:
+        money_capital -= deficit
+    spend *= (1 + increase)
+    if money_capital < 0:
+        break
+    months += 1
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
---
+++ Лабораторная работа 2/Task2.py
@@ -0,0 +1,16 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+money_capital = 0
+
+for month in range(1, months + 1):
+    deficit = spend - salary
+    if deficit > 0:
+        money_capital += deficit
+        spend *= (1 + increase)
+
+
+money_capital = round(money_capital)
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
